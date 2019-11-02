2200951a4409a48e8ba27c6afc58f7674a63aaad
Johan Snider <johan.snider@gmail.com>
2019-11-02 17:20:05 +0100

    addedhttp request with volley

--- app/src/main/java/se/mjukis/redcrossaction/MainActivity.kt
+++ app/src/main/java/se/mjukis/redcrossaction/MainActivity.kt
@@ -2,6 +2,10 @@ package se.mjukis.redcrossaction
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Response
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            textView.text = "Hello"
+            //from: https://developer.android.com/training/volley/simple.html#kotlin
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://www.google.com"
+
+            // Request a string response from the provided URL.
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    textView.text = "Response is: ${response.substring(0, 500)}"
+                },
+                Response.ErrorListener { textView.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
+
+
         }
     }
 }
