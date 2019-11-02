20939a0acba19cdc0d11db05eb5cd0db2499b714
Lena Shervarly <lena.shervarly@frisq.se>
2019-11-02 16:24:13 +0100

    add severity status update page view

--- app/src/main/java/se/mjukis/redcrossaction/SeverityStatusActivity.kt
+++ app/src/main/java/se/mjukis/redcrossaction/SeverityStatusActivity.kt
@@ -3,12 +3,10 @@ package se.mjukis.redcrossaction
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SeverityStatusActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
+        setContentView(R.layout.activity_severity_status)
     }
 }
