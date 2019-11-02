3f458be064a33996f4a0ddfb35ec0c2c53de6a79
Lena Shervarly <lena.shervarly@frisq.se>
2019-11-02 20:40:07 +0100

    upgrade project to use firebase and material design and update design of severity status

--- app/src/main/java/se/mjukis/redcrossaction/SeverityStatusActivity.kt
+++ app/src/main/java/se/mjukis/redcrossaction/SeverityStatusActivity.kt
@@ -1,7 +1,8 @@
 package se.mjukis.redcrossaction
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SeverityStatusActivity : AppCompatActivity() {
 
